76926e29ffd4dee440c6e1a6347806873e04406c
NixonShih <powerwolf543@gmail.com>
2018-07-23 22:39:46 +0800

    - updated 1_TwoSum

--- 001_TwoSun/TwoSum_Nixon.playground/Contents.swift
+++ 001_TwoSun/TwoSum_Nixon.playground/Contents.swift
@@ -7,7 +7,49 @@ struct Element {
 }
 
 class Solution {
+    
     func twoSum(_ nums: [Int], _ target: Int) -> [Int] {
+        
+        var dic: [Int: Int] = [:]
+
+        for i in 0..<nums.count {
+            let num = nums[i]
+            let complement = target - num
+            guard let j = dic[complement] else {
+                dic[num] = i
+                continue
+            }
+            return [i, j]
+        }
+        
+        return [-1, -1]
+    }
+
+    
+    func aTwoSum(_ nums: [Int], _ target: Int) -> [Int] {
+        
+        var result: [Int]?
+        var dic: [Int: Int] = [:]
+        
+        for i in 0..<nums.count {
+            dic[nums[i]] = i
+        }
+
+        for i in 0..<nums.count {
+            let num = nums[i]
+            let difference = target - num
+            guard let j = dic[difference], i != j else {
+                continue
+            }
+            result = [i, j]
+            break
+        }
+        
+        return result ?? [0, 0];
+    }
+
+    
+    func bTwoSum(_ nums: [Int], _ target: Int) -> [Int] {
         
         let elements = nums.enumerated().map{ Element(value: $1, index: $0) }.sorted { $0.value < $1.value }
         var startIndex = 0;
@@ -35,6 +77,5 @@ class Solution {
     }
 }
 
-
-let input = [3,2,5]
-Solution().twoSum(input, 8);
+let input = [3,2,4]
+Solution().twoSum(input, 6);
